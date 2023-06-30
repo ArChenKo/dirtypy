8a83c00a8452d3bb4948908cccc9127d387934dc
Arslan <arslan.chen@mail.ru>
2023-06-30 23:19:37 +0400

    ex07 added

--- ex07.py
+++ ex07.py
@@ -1,2 +1,12 @@
 # Проверьте, содержит ли данный список из n чисел, все числа от 1 до n.
 
+from random import randint
+
+num = int(input('Введите число n: '))
+print(spisok := [randint(1, num) for _ in range(num)])
+
+m_spisok=[i for i in range(1,num+1)]
+print(m_spisok)
+for i in m_spisok:
+    if i not in spisok:
+        print(i, end=' ')
